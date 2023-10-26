2f5b274673d6c78dd41156dcdcc76dd88e2bb0ee
mainul96 <110577655+mainul96@users.noreply.github.com>
2023-10-26 09:48:34 +0530

    add remove from localstorage

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -2,18 +2,27 @@ import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import ReviewItems from '../ReviewItems/ReviewItems';
+import { deleteFromDb } from '../../utilities/fakedb';
 
 const Orders = () => {
     const {products, initialCart} = useLoaderData();
     const [cart, setCart] = useState(initialCart);
+
+    const handleRemoveItem = id =>{
+       const remaining = cart.filter(product => product.key !== id);
+       setCart(remaining);
+       deleteFromDb(id);
+    }
+    
     return (
         <div className='shop-container'>
             <div className='product-container'>
                 
                 {
                     cart.map(product => <ReviewItems
-                    key={product.key}
-                    product ={product}
+                    key = {product.key}
+                    product = {product}
+                    handleRemoveItem = {handleRemoveItem}
                     >  </ReviewItems>)
                 }
             </div>
--- src/components/ReviewItems/ReviewItems.js
+++ src/components/ReviewItems/ReviewItems.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './ReviewItems.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
-const ReviewItems = ({ product }) => {
-    const { name, img, quantity, price } = product;
-    console.log(product)
+const ReviewItems = ({ product, handleRemoveItem }) => {
+    const { key, name, img, quantity, price } = product;
+
     return (
         <div className='review-item'>
             <div>
@@ -11,11 +13,14 @@ const ReviewItems = ({ product }) => {
             </div>
             <div className='review-details-container'>
                 <div className='review-container'>
+                    <p>{name.slice(0, 10)}</p>
                   <p>Price: {price}</p>
                   <p>Quantity: {quantity}</p>
                 </div>
                 <div className='delete-container'>
-                        <button>Delete</button>
+                        <button onClick={()=>handleRemoveItem(key)} className='btn-delete'>
+                            <FontAwesomeIcon className='delete-icon' icon={faTrashAlt}></FontAwesomeIcon>
+                        </button>
                 </div>
 
             </div>
