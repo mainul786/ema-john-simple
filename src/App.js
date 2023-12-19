219610ae899a8d6d404fb241ba4b31c11ef0d373
mainul96 <110577655+mainul96@users.noreply.github.com>
2023-12-19 22:06:13 +0530

    pegination and data show

--- src/App.js
+++ src/App.js
@@ -17,44 +17,44 @@ import PrivateRoutes from './Router/PrivateRoutes';
 function App() {
   const router = createBrowserRouter([
     {
-      path:'/',
-      element:<Main></Main>,
-      children:[
-        {path:'/',
-        loader:()=> fetch(`http://localhost:5000/products`),
-        element:<Shop></Shop>
-      },
-      {
-        path:'orders',
-        loader: productsAndCartLoader,
-        element:<Orders></Orders>
-      },
-      {
-        path:'inventory',
-        element:<Inventory></Inventory>
-      },
-      {
-        path:'shipping',
-        element:<PrivateRoutes><Shipping></Shipping></PrivateRoutes>
-      },
-      {
-        path:'about',
-        element:<About></About>
-      },
-      {
-        path:'login',
-        element:<Login></Login>
-      },
-      {
-        path:'signUp',
-        element:<SignUp></SignUp>
-      }
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Shop></Shop>
+        },
+        {
+          path: 'orders',
+          loader: productsAndCartLoader,
+          element: <Orders></Orders>
+        },
+        {
+          path: 'inventory',
+          element: <Inventory></Inventory>
+        },
+        {
+          path: 'shipping',
+          element: <PrivateRoutes><Shipping></Shipping></PrivateRoutes>
+        },
+        {
+          path: 'about',
+          element: <About></About>
+        },
+        {
+          path: 'login',
+          element: <Login></Login>
+        },
+        {
+          path: 'signUp',
+          element: <SignUp></SignUp>
+        }
       ]
     }
   ])
   return (
     <div>
-     <RouterProvider router={router}></RouterProvider>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,17 +3,30 @@ import './Shop.css';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
 import { addToDb, clearTheCart, getStoredCart } from '../../utilities/fakedb';
-import { Link, useLoaderData } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+
 
 
 
 
 
 const Shop = () => {
-    const {products, count} = useLoaderData();
+    const [products, setProducts] = useState([]);
+    const [count, setCount] = useState(0)
     const [cart, setCart] = useState([]);
     const [page, setPage] = useState(0);
     const [size, setSize] = useState(10);
+
+    useEffect(()=>{
+        const url = `http://localhost:5000/products?page=${page}&size=${size}`;
+        fetch(url)
+        .then(res=> res.json())
+        .then(data=>{
+            setCount(data.count);
+            setProducts(data.products);
+        })
+    }, [page, size])
 
     const pages = Math.ceil(count / size);
 
@@ -26,8 +39,21 @@ const Shop = () => {
        const storedCart =  getStoredCart();
     //    console.log(storedCart);
     const savedCart = [];
-       for(const id in storedCart){
-        const addedProduct = products.find(product => product._id === id);
+    const ids = Object.keys(storedCart);
+    console.log(ids)
+
+    fetch(`http://localhost:5000/productsByIds`,{
+        method:'POST',
+        headers:{
+            'content-type':'application/json'
+        },
+        body:JSON.stringify(ids)
+    })
+    .then(res=> res.json())
+    .then(data => {
+        console.log(data)
+           for(const id in storedCart){
+        const addedProduct = data.find(product => product._id === id);
         // console.log(addedProduct)
         if(addedProduct){
             const quantity = storedCart[id];
@@ -37,6 +63,8 @@ const Shop = () => {
        }
 
        setCart(savedCart);
+    })
+
     }, [products])
 
     const handelAddToCart = selectedProduct =>{
@@ -76,14 +104,20 @@ const Shop = () => {
                 </Cart>
             </div>
            <div className='pegination'>
-            <p>Current Page : {page}</p>
+            <p>Current Page : {page} and size {size}</p>
            {
                 [...Array(pages).keys()].map(number=><button
                 key={number}
                 className={page === number && 'selected'}
                 onClick={()=>setPage(number)}
-                >{number}</button>)
+                >{number+1}</button>)
             }
+            <select onChange={event => setSize(event.target.value)}>
+                <option value="5">5</option>
+                <option value="10" selected>10</option>
+                <option value="15">15</option>
+                <option value="20">20</option>
+            </select>
            </div>
         </div>
         
